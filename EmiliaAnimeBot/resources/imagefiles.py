34fe8364dce46d6de7b0c17185e6bf2813f89f65
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-11-22 17:32:09 +0800

    Update imagefiles.py

--- EmiliaAnimeBot/resources/imagefiles.py
+++ EmiliaAnimeBot/resources/imagefiles.py
@@ -1,6 +1,6 @@
 # This File Contains all the Picture Links used all over the Bot
 
 #Images in __main__.py
-EMILIA_IMG = "https://telegra.ph/file/b37cec509d121c8c63518.jpg"
-EMILIA_START_IMG = "https://telegra.ph/file/e5fc94ada18369f89c832.jpg"
-EMILIA_HELP_IMG = "https://telegra.ph/file/65fb4fdec1bd9787f01a0.jpg"
+EMILIA_IMG = "https://telegra.ph/file/62a8fd1f95fa5a8d6c3f6.jpg"
+EMILIA_START_IMG = "https://telegra.ph/file/62a8fd1f95fa5a8d6c3f6.jpg"
+EMILIA_HELP_IMG = "https://telegra.ph/file/44b20ac91561950fed728.jpg"
